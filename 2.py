26ded69ae8ce65482fda6a583295edeed54d60e1
MartinaSotelo <martinaperezsotelo@gmail.com>
2025-10-30 02:20:02 -0300

    Comment out sections and add balance query

--- 2.py
+++ 2.py
@@ -1,3 +1,13 @@
+
+"""
+Grupo Import_milanesas
+Integrantes: 
+Dulio Joaquin, 
+Risuleo Franco, 
+Perez Sotelo Martina
+
+"""
+
 import pandas as pd
 import duckdb as dd
 import re
@@ -14,22 +24,22 @@ kuzu_full = pd.read_csv(carpeta+"kuzushiji_full.csv")
 import pandas as pd
 import matplotlib.pyplot as plt
 
-X = píxeles (todas menos la última columna)
+#X = píxeles (todas menos la última columna)
 X = kuzu_full.iloc[:, :-1].values
 
-y = etiquetas (última columna)
+#y = etiquetas (última columna)
 y = kuzu_full.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
 
-Dividimos los datos
+#Dividimos los datos
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
 X_train_imgs = X_train.reshape(-1, 28, 28)
 
-Mostramos las primeras 9 imágenes
+#Mostramos las primeras 9 imágenes
 fig, axes = plt.subplots(3, 3, figsize=(6, 6))
 
 for i, ax in enumerate(axes.flat):
@@ -51,7 +61,7 @@ kuzu_full_4_5 = dd.query(consulta).df()
 
 X = kuzu_full_4_5.iloc[:, :-1].values
 
-y = etiquetas (última columna)
+#y = etiquetas (última columna)
 y = kuzu_full_4_5.iloc[:, -1].values
 
 from sklearn.model_selection import train_test_split
@@ -62,7 +72,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 X_train_imgs = X_train.reshape(-1, 28, 28)
 
-Mostramos las primeras 9 imágenes
+#Mostramos las primeras 9 imágenes
 fig, axes = plt.subplots(3, 3, figsize=(6, 6))
 
 for i, ax in enumerate(axes.flat):
@@ -74,9 +84,11 @@ plt.tight_layout()
 plt.show()
 #%%
 
+#Con esta consulta vemos que estan balanceados los datos. Hay 70000 de c/u
+
 consulta = """
            SELECT COUNT(label)
            FROM kuzu_full_4_5
            GROUP BY label
            """
-a = dd.query(consulta).df()
+balance = dd.query(consulta).df()
