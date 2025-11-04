73103d61dfed02afdd1ba7b5dede7a5a4a044f4b
franco-ris <risuleofranco@gmail.com>
2025-11-04 20:24:22 -0300

    Improve KNN model training and accuracy tracking
    Refactor KNN model training with attribute subsets and neighbor selection.

--- 2.py
+++ 2.py
@@ -10,11 +10,8 @@ Perez Sotelo Martina
 
 import pandas as pd
 import duckdb as dd
-import re
-import numpy as np
-import sklearn
 
-carpeta = ""
+carpeta = "C:/Users/risul/OneDrive/Escritorio/TP2/"
 
 kminst = pd.read_csv(carpeta+"kmnist_classmap_char.csv")
 kuzu_full = pd.read_csv(carpeta+"kuzushiji_full.csv")
@@ -94,6 +91,27 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, accuracy_score
 import random
 
+#Vemos que los bordes suelen dar peor que los atributos más centrados
+modelos_knnC = pd.DataFrame(columns=["Cantidad_atributos", "Atributos_usados", "Accuracy"])
+#Bordes-bordes-medio-seguidos-cualquiercosa
+subconjuntos_de_atributos4 = [[0,27,28,55],[56,83,84,168],[17,62,92,120],[444,445,446,447],[578,612,499,702]]
+subconjuntos_de_atributos5 = [[0,27,28,55,56],[56,83,84,168,783],[17,62,92,120,355],[444,445,446,447,448],[578,612,499,702,550]]
+subconjuntos_de_atributos7 = [[0,27,28,55,56,83,84],[56,83,84,168,196,224,252],[17,62,92,120,264,148,404],[444,445,446,447,448,449,450],[578,612,499,702,123,389,12]]
+s = [subconjuntos_de_atributos4, subconjuntos_de_atributos5, subconjuntos_de_atributos7]
+for grupo_subconjuntos in s:
+    cant_atributos = len(grupo_subconjuntos[0])
+    for atributos in grupo_subconjuntos:
+        sub_X_train = X_train[:, atributos]
+        sub_X_test = X_test[:, atributos]
+        knn = KNeighborsClassifier()
+        knn.fit(sub_X_train, y_train)
+        y_pred = knn.predict(sub_X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        modelos_knnC.loc[len(modelos_knnC)] = [cant_atributos, atributos, accuracy]
+
+
+
+#%%
 #Este código genera atributos aleatorios, entrena un knn en base a esos atributos (testea el mejor n
 #en enes y usa ese), y guarda cómo le fue a cada modelo (accuracy) con sus atributos elegidos, junto con la cantidad
 #de atributos que usó y el mejor n. A lo mejor en vez de testear el mejor n, habría que guardar todos y encontrar
@@ -101,7 +119,6 @@ import random
 #terminan siendo los n chicos?
 
 n_atributos = [3,4,5,8,10,15,20]
-
 modelos_knn = pd.DataFrame(columns=["Cantidad_atributos", "Atributos_usados", "Mejor_n", "Accuracy"])
 enes = [4,10,20] #Habría que ver qué números de vecinos probar. Puse esos de prueba nomas
 for cant_atributos in n_atributos:
@@ -114,7 +131,7 @@ for cant_atributos in n_atributos:
         mejor_n = enes[0]
         sub_X_train = X_train[:, atributos]
         sub_X_test = X_test[:, atributos]
-        for n in enes:
+        for n in range(2,30,5):
             knn = KNeighborsClassifier(n_neighbors=n)
             knn.fit(sub_X_train, y_train)
             y_pred = knn.predict(sub_X_test)
