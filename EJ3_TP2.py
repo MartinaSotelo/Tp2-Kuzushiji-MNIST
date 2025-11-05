30659eca62b7692265c6d49b8a20ca4b3f495e2b
JoacoDulio <165932622+JoaquinDulio@users.noreply.github.com>
2025-11-05 20:33:13 -0300

    EJ3_TP2.py

--- EJ3_TP2.py
+++ EJ3_TP2.py
@@ -19,7 +19,7 @@ y = kuzu_full['label']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
-
+#%% 3b)
 data = pd.DataFrame(columns=['profundidad','presicion'])
 for i in range(1,10,2):
     arbol = DecisionTreeClassifier(criterion='gini',
@@ -40,3 +40,21 @@ for i in range(1,10,2):
 print("Precisión en cada fold:", scores)
 print("Precisión promedio:", scores.mean())
 
+#%% 3c)
+
+lista = ['gini','entropy']
+data1 = pd.DataFrame(columns=['profundidad','criterio','min_samples_split','min_samples_leaf','presicion'])
+for i in range(1,11,3):
+    for j in range(2,20,5):
+        for k in range(1,11,3):
+            for elem in lista:
+                arbol = DecisionTreeClassifier(criterion=elem,
+                splitter='best',
+                max_depth=i,
+                min_samples_split=j,
+                min_samples_leaf=k,
+                max_features=None,
+                random_state=2)
+
+                scores = cross_val_score(arbol, X_train, y_train, cv=5)
+                data1.loc[len(data1)]=[i,elem,j,k,scores.mean()] 
