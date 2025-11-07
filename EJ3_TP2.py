0fc9931d3f7d9adb1310d52c1c43cfc61d217cb9
JoacoDulio <165932622+JoaquinDulio@users.noreply.github.com>
2025-11-07 19:02:04 -0300

    EJ3_TP2.py

--- EJ3_TP2.py
+++ EJ3_TP2.py
@@ -20,8 +20,8 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 #%% 3b)
-data = pd.DataFrame(columns=['Profundidad','Precisión'])
-for i in range(1,10,2):
+data = pd.DataFrame(columns=['profundidad','presicion'])
+for i in range(1,11,3):
     arbol = DecisionTreeClassifier(criterion='gini',
         splitter='best',
         max_depth=i,
@@ -32,13 +32,26 @@ for i in range(1,10,2):
 
     scores = cross_val_score(arbol, X_train, y_train, cv=5)
     data.loc[len(data)]=[i,scores.mean()]
+# arbol.fit(X_train, y_train) # Entrenamiento del modelo
+# prediction = arbol.predict(X_test) # Generamos las predicciones // llamamos al modelo
+# #print(prediction)
+# accuracy = accuracy_score(y_test, prediction)
 
+print("Precisión en cada fold:", scores)
+print("Precisión promedio:", scores.mean())
+
+# Luego de realizar varias pruebas, se llega a la conclusion de que la presicion máxima es de aproximadamente el 73%
+# con max_depth = 30, el accuracy es del 0.733%, mientras que en max_depth = 38, era del 0.735%
+# Y mas o menos a partir de profundidad 40, es que el accuracy deja de subir
 #%% 3c)
-data1 = pd.DataFrame(columns=['Profundidad','Criterio','min_samples_split','min_samples_leaf','Precisión'])
+# !!Este codigo tarda mucho en correr!! 
+
+lista = ['gini','entropy']
+data1 = pd.DataFrame(columns=['profundidad','criterio','min_samples_split','min_samples_leaf','presicion'])
 for i in range(1,11,3):
     for j in range(2,20,5):
         for k in range(1,11,3):
-            for elem in ['gini','entropy']:
+            for elem in lista:
                 arbol = DecisionTreeClassifier(criterion=elem,
                 splitter='best',
                 max_depth=i,
@@ -48,5 +61,30 @@ for i in range(1,11,3):
                 random_state=2)
 
                 scores = cross_val_score(arbol, X_train, y_train, cv=5)
-                data1.loc[len(data1)]=[i,elem,j,k,scores.mean()] 
+                data1.loc[len(data1)]=[i,elem,j,k,scores.mean()]  
+                
+
+
+#%% 3d)
+data = pd.DataFrame(columns=['profundidad','presicion'])
+arbol = DecisionTreeClassifier(criterion='entropy',
+        splitter='best',
+        max_depth=10,
+        min_samples_split=2,
+        min_samples_leaf=1,
+        max_features=None,
+        random_state=None)
+    
+arbol.fit(X_train, y_train) # Entrenamiento del modelo
+prediction = arbol.predict(X_test) # Generamos las predicciones // llamamos al modelo
+#print(prediction)
+accuracy = accuracy_score(y_test, prediction)
+
+#%%
+
+from sklearn.metrics import confusion_matrix
+
+prediction = arbol.predict(X_test)
+matriz = confusion_matrix(y_test, prediction)
+print(matriz)
 
