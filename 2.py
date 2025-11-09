6d08159c80b7c3afac91fe88496825e0e9d0fb1a
franco-ris <risuleofranco@gmail.com>
2025-11-09 19:56:09 -0300

    Refactor KNN attribute subsets and clean up code
    Updated attribute subsets for KNN model evaluation and removed unused code.

--- 2.py
+++ 2.py
@@ -14,7 +14,7 @@ de las clases 4 y 5.
 import pandas as pd
 import duckdb as dd
 
-carpeta = "/home/martina/import_milanesas_TP02/"
+carpeta = ""
 
 kminst = pd.read_csv(carpeta+"kmnist_classmap_char.csv")
 kuzu_full = pd.read_csv(carpeta+"kuzushiji_full.csv")
@@ -94,38 +94,12 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report, accuracy_score
 import random
 
-
-#armar un conjunto de pixeles que contenga a estos. Ver que pasa. Yo creo que tendria que dar mejor la prediccion...
-"""los pixeles con mayor variabilidad entre clase 4 y clase 5 son:
-122:111.02075871986933
-123:111.9380869547499
-150:111.61850063989782
-151:112.16713747463827
-178:111.71238033694596
-179:111.39979353322525
-331:111.5050539917653
-342:111.50445185067507
-343:111.3912744033587
-359:111.74018106972679
-360:113.12511998004403
-370:112.56159549792135
-371:111.91295842000963
-388:113.56507449800914
-389:111.72072661315468
-398:112.29384775491697
-399:111.07586680489429
-416:112.29931665864497
-417:112.63029018562779
-426:111.66827645042022
-445:111.12800850129747"""
-
-
 #Vemos que los bordes suelen dar peor que los atributos más centrados
 modelos_knnC = pd.DataFrame(columns=["Cantidad_atributos", "Atributos_usados", "Accuracy"])
-#Bordes-bordes-medio-seguidos-cualquiercosa-atributos con mayor variabilidad.
-subconjuntos_de_atributos4 = [[0,27,28,55],[56,83,84,168],[17,62,92,120],[444,445,446,447],[578,612,499,702],[416,370,360,388]]
-subconjuntos_de_atributos5 = [[0,27,28,55,56],[56,83,84,168,783],[17,62,92,120,355],[444,445,446,447,448],[578,612,499,702,550],[416,151,370,360,388]]
-subconjuntos_de_atributos7 = [[0,27,28,55,56,83,84],[56,83,84,168,196,224,252],[17,62,92,120,264,148,404],[444,445,446,447,448,449,450],[578,612,499,702,123,389,12],[151,388,398,416,370,360,388]]
+#Bordes-bordes-medio-seguidos-cualquiercosa-atributos con mayor variabilidad-atributos con mayor variabilidad entre clases pero baja dentro de una misma (fisher score).
+subconjuntos_de_atributos4 = [[0,27,28,55],[56,83,84,168],[17,62,92,120],[444,445,446,447],[578,612,499,702],[416,370,360,388],[94,122,149,150]]
+subconjuntos_de_atributos5 = [[0,27,28,55,56],[56,83,84,168,783],[17,62,92,120,355],[444,445,446,447,448],[578,612,499,702,550],[416,151,370,360,388],[177,178,205,206,122]]
+subconjuntos_de_atributos7 = [[0,27,28,55,56,83,84],[56,83,84,168,196,224,252],[17,62,92,120,264,148,404],[444,445,446,447,448,449,450],[578,612,499,702,123,389,12],[151,388,398,416,370,360,389],[149,178,206,150,94,205,122]]
 s = [subconjuntos_de_atributos4, subconjuntos_de_atributos5, subconjuntos_de_atributos7]
 for grupo_subconjuntos in s:
     cant_atributos = len(grupo_subconjuntos[0])
@@ -138,8 +112,6 @@ for grupo_subconjuntos in s:
         accuracy = accuracy_score(y_test, y_pred)
         modelos_knnC.loc[len(modelos_knnC)] = [cant_atributos, atributos, accuracy]
 
-
-
 #%%
 #Este código genera atributos aleatorios, entrena un knn en base a esos atributos (testea el mejor n
 #en enes y usa ese), y guarda cómo le fue a cada modelo (accuracy) con sus atributos elegidos, junto con la cantidad
@@ -149,15 +121,15 @@ for grupo_subconjuntos in s:
 
 n_atributos = [3,4,5,8,10,15,20]
 modelos_knn = pd.DataFrame(columns=["Cantidad_atributos", "Atributos_usados", "Mejor_n", "Accuracy"])
-enes = [4,10,20] #Habría que ver qué números de vecinos probar. Puse esos de prueba nomas
+
 for cant_atributos in n_atributos:
     subconjuntos_de_atributos = []
-    for i in range(6):
+    for i in range(10):
         subconjunto = random.sample(range(0, 784), cant_atributos)
         subconjuntos_de_atributos.append(subconjunto)
     for atributos in subconjuntos_de_atributos:
         mejor_accuracy = 0
-        mejor_n = enes[0]
+        mejor_n = 2
         sub_X_train = X_train[:, atributos]
         sub_X_test = X_test[:, atributos]
         for n in range(2,30,5):
@@ -169,36 +141,7 @@ for cant_atributos in n_atributos:
                 mejor_accuracy = accuracy
                 mejor_n = n
         modelos_knn.loc[len(modelos_knn)] = [cant_atributos, atributos, mejor_n, mejor_accuracy]
-
-
-
-
-#Cómo me dijo chatgpt que lo hiciera con cross validation. Capaz sirve para el 3
-#from sklearn.model_selection import cross_val_score, StratifiedKFold
-
-# modelos_knn_cross_validation = pd.DataFrame(columns=["Atributos_usados", "Mejor_numero_neighbors", "Accuracy"])
-# cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
-
-# for atributos in subconjuntos_de_atributos:
-#     mejor_accuracy = 0
-#     mejor_n = enes[0]
-#     sub_X_train = X_train[:, atributos]
-#     sub_X_test = X_test[:, atributos]
-#     for n in enes:
-#         knn = KNeighborsClassifier(n_neighbors=n)
-#         scores = cross_val_score(knn, sub_X_train, y_train, cv=cv, scoring='accuracy', n_jobs=-1)
-#         mean_accuracy = scores.mean()
-#         if mean_accuracy > mejor_accuracy:
-#             mejor_accuracy = mean_accuracy
-#             mejor_n = n
-#     knn_final = KNeighborsClassifier(n_neighbors=mejor_n)
-#     knn_final.fit(sub_X_train, y_train)
-#     test_acc = knn_final.score(sub_X_test, y_test)
-#     modelos_knn_cross_validation.loc[len(modelos_knn_cross_validation)] = [atributos, mejor_n, mejor_accuracy]
-    
-#con cross validation o no dan bastante parecidos igual
-
-#Usé accuracy, no veo por qué otra métrica sería más adecuada
+               
 # %%
 
 #copie y pegue de gemini:
